b5174fa22cba25ce3108bc268bef7cc54a713aa3
yanggy <ilike.yanggy@gmail.com>
2024-04-05 20:19:13 +0800

    init

---
+++ spring-cloud-openfeign-core/src/main/java/org/springframework/cloud/openfeign/FeignClientsRegistrar.java
@@ -0,0 +1,477 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.openfeign;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.AbstractClassTestingTypeFilter;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * ImportBeanDefinitionRegistrar：用于向容器中注入bean 关注其 registerBeanDefinitions 方法
+ *
+ * @author Spencer Gibb
+ * @author Jakub Narloch
+ * @author Venil Noronha
+ * @author Gang Li
+ */
+class FeignClientsRegistrar
+		implements ImportBeanDefinitionRegistrar, ResourceLoaderAware, EnvironmentAware {
+
+	// patterned after Spring Integration IntegrationComponentScanRegistrar
+	// and RibbonClientsConfigurationRegistgrar
+
+	private ResourceLoader resourceLoader;
+
+	private Environment environment;
+
+	FeignClientsRegistrar() {
+	}
+
+	static void validateFallback(final Class clazz) {
+		Assert.isTrue(!clazz.isInterface(),
+				"Fallback class must implement the interface annotated by @FeignClient");
+	}
+
+	static void validateFallbackFactory(final Class clazz) {
+		Assert.isTrue(!clazz.isInterface(), "Fallback factory must produce instances "
+				+ "of fallback classes that implement the interface annotated by @FeignClient");
+	}
+
+	static String getName(String name) {
+		if (!StringUtils.hasText(name)) {
+			return "";
+		}
+
+		String host = null;
+		try {
+			String url;
+			if (!name.startsWith("http://") && !name.startsWith("https://")) {
+				url = "http://" + name;
+			}
+			else {
+				url = name;
+			}
+			host = new URI(url).getHost();
+
+		}
+		catch (URISyntaxException e) {
+		}
+		Assert.state(host != null, "Service id not legal hostname (" + name + ")");
+		return name;
+	}
+
+	static String getUrl(String url) {
+		if (StringUtils.hasText(url) && !(url.startsWith("#{") && url.contains("}"))) {
+			if (!url.contains("://")) {
+				url = "http://" + url;
+			}
+			try {
+				new URL(url);
+			}
+			catch (MalformedURLException e) {
+				throw new IllegalArgumentException(url + " is malformed", e);
+			}
+		}
+		return url;
+	}
+
+	static String getPath(String path) {
+		if (StringUtils.hasText(path)) {
+			path = path.trim();
+			if (!path.startsWith("/")) {
+				path = "/" + path;
+			}
+			if (path.endsWith("/")) {
+				path = path.substring(0, path.length() - 1);
+			}
+		}
+		return path;
+	}
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
+	/**
+	 * bean注入的 入口函数
+	 * @param metadata
+	 * @param registry
+	 */
+	@Override
+	public void registerBeanDefinitions(AnnotationMetadata metadata,
+			BeanDefinitionRegistry registry) {
+		// 完成 Feign 框架相关的配置注册
+		registerDefaultConfiguration(metadata, registry);
+		// 注册由 @FeignClient 修饰的接口 bean *****核心*****
+		registerFeignClients(metadata, registry);
+	}
+
+	/**
+	 * 向容器中注册由 @FeignClient 修饰的接口bean
+	 * @param metadata 包含了@EnableFeignClients 注解的元信息
+	 * @param registry
+	 */
+	public void registerFeignClients(AnnotationMetadata metadata,
+			BeanDefinitionRegistry registry) {
+		// ClassPath Scanner
+		ClassPathScanningCandidateComponentProvider scanner = getScanner();
+		scanner.setResourceLoader(this.resourceLoader);
+		// 接收 @EnableFeignClients(basePackages = {"com.itheima.driver.feign"})
+		Set<String> basePackages;
+		Map<String, Object> attrs = metadata
+				.getAnnotationAttributes(EnableFeignClients.class.getName());
+		AnnotationTypeFilter annotationTypeFilter = new AnnotationTypeFilter(
+				FeignClient.class);
+		final Class<?>[] clients = attrs == null ? null
+				: (Class<?>[]) attrs.get("clients");
+		if (clients == null || clients.length == 0) {
+			scanner.addIncludeFilter(annotationTypeFilter);
+			// 获取 @EnableFeignClients中配置的 @FeignClient 接口扫描路径
+			basePackages = getBasePackages(metadata);
+		}
+		else {
+			final Set<String> clientClasses = new HashSet<>();
+			basePackages = new HashSet<>();
+			for (Class<?> clazz : clients) {
+				basePackages.add(ClassUtils.getPackageName(clazz));
+				clientClasses.add(clazz.getCanonicalName());
+			}
+			AbstractClassTestingTypeFilter filter = new AbstractClassTestingTypeFilter() {
+				@Override
+				protected boolean match(ClassMetadata metadata) {
+					String cleaned = metadata.getClassName().replaceAll("\\$", ".");
+					return clientClasses.contains(cleaned);
+				}
+			};
+			scanner.addIncludeFilter(
+					new AllTypeFilter(Arrays.asList(filter, annotationTypeFilter)));
+		}
+
+		// 拿到 @EnableFeignClients 中配置的 @FeignClient 接口扫描路径 后开始 扫描
+		for (String basePackage : basePackages) {
+			// 查找 basePackage 包路径下所有 由 @FeignClient 修饰的候选bean,返回其 BeanDefinition 的集合
+			Set<BeanDefinition> candidateComponents = scanner
+					.findCandidateComponents(basePackage);
+
+			// 针对每个标注了 @FeignClient 的候选 BeanDefinition (接口的BeanDefinition) 准备向容器中注册
+			for (BeanDefinition candidateComponent : candidateComponents) {
+				if (candidateComponent instanceof AnnotatedBeanDefinition) {
+					// verify annotated class is an interface
+					AnnotatedBeanDefinition beanDefinition = (AnnotatedBeanDefinition) candidateComponent;
+					AnnotationMetadata annotationMetadata = beanDefinition.getMetadata();
+					Assert.isTrue(annotationMetadata.isInterface(),
+							"@FeignClient can only be specified on an interface");// @FeignClient标注的必须是接口
+					// 获取 @FeignClient 注解的相关属性信息
+					Map<String, Object> attributes = annotationMetadata
+							.getAnnotationAttributes(
+									FeignClient.class.getCanonicalName());
+					// 获取@FeignClient(value =
+					// "hailtaxi-driver"),name属性,name属性和value属性是相同的含义,都是配置服务名
+					String name = getClientName(attributes);// name = hailtaxi-driver
+					registerClientConfiguration(registry, name,
+							attributes.get("configuration"));
+					// 针对当前标注了 @FeignClient 注解的候选接口 BeanDefinition 向容器中注册bean信息
+					registerFeignClient(registry, annotationMetadata, attributes);
+				}
+			}
+		}
+	}
+
+	private void registerDefaultConfiguration(AnnotationMetadata metadata,
+			BeanDefinitionRegistry registry) {
+		Map<String, Object> defaultAttrs = metadata
+				.getAnnotationAttributes(EnableFeignClients.class.getName(), true);
+
+		if (defaultAttrs != null && defaultAttrs.containsKey("defaultConfiguration")) {
+			String name;
+			if (metadata.hasEnclosingClass()) {
+				name = "default." + metadata.getEnclosingClassName();
+			}
+			else {
+				name = "default." + metadata.getClassName();
+			}
+			registerClientConfiguration(registry, name,
+					defaultAttrs.get("defaultConfiguration"));
+		}
+	}
+
+	/**
+	 * 向容器中注册 每个标注了 @FeignClient 的接口bean
+	 * @param registry
+	 * @param annotationMetadata
+	 * @param attributes
+	 */
+	private void registerFeignClient(BeanDefinitionRegistry registry,
+			AnnotationMetadata annotationMetadata, Map<String, Object> attributes) {
+		// 接口全路径
+		String className = annotationMetadata.getClassName();// className=com.itheima.driver.feign.DriverFeign
+		/**
+		 * 每个标注了@FeignClient 的接口，真正向容器中注册的其实是一个 FeignClientFactoryBean 1、创建
+		 * FeignClientFactoryBean的 BeanDefinition 2、向 BeanDefinition
+		 * 中填充相关属性，属性来源于接口上@FeignClient的属性信息
+		 */
+		BeanDefinitionBuilder definition = BeanDefinitionBuilder
+				.genericBeanDefinition(FeignClientFactoryBean.class);
+		validate(attributes);// 验证fallback和fallbackFactory是不是接口
+
+		definition.addPropertyValue("url", getUrl(attributes));
+		definition.addPropertyValue("path", getPath(attributes));
+		String name = getName(attributes);
+		definition.addPropertyValue("name", name);
+		String contextId = getContextId(attributes);
+		definition.addPropertyValue("contextId", contextId);
+		definition.addPropertyValue("type", className); // 把接口全路径也设置到 definition
+		definition.addPropertyValue("decode404", attributes.get("decode404"));
+		definition.addPropertyValue("fallback", attributes.get("fallback"));
+		definition.addPropertyValue("fallbackFactory", attributes.get("fallbackFactory"));
+		definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+
+		String alias = contextId + "FeignClient";
+		//
+		AbstractBeanDefinition beanDefinition = definition.getBeanDefinition();
+
+		boolean primary = (Boolean) attributes.get("primary"); // has a default, won't be
+																// null
+		beanDefinition.setPrimary(primary);
+		String qualifier = getQualifier(attributes);
+		if (StringUtils.hasText(qualifier)) {
+			alias = qualifier;
+		}
+		BeanDefinitionHolder holder = new BeanDefinitionHolder(beanDefinition, className,
+				new String[] { alias });
+		BeanDefinitionReaderUtils.registerBeanDefinition(holder, registry);
+		/**
+		 * 由于标注了@FeignClient的每个接口真正向容器中注册时注册的是与该接口相关的：FeignClientFactoryBean 而
+		 * FeignClientFactoryBean 实现了 FactoryBean 接口，也就是说当需要从容器中获取
+		 * 这个bean时，获取出来的bean其实是由它的getObject方法返回的bean
+		 *
+		 * 所以：下一个入口是： FeignClientFactoryBean#getObject
+		 */
+
+	}
+
+	private void validate(Map<String, Object> attributes) {
+		AnnotationAttributes annotation = AnnotationAttributes.fromMap(attributes);
+		// This blows up if an aliased property is overspecified
+		// FIXME annotation.getAliasedString("name", FeignClient.class, null);
+		validateFallback(annotation.getClass("fallback"));
+		validateFallbackFactory(annotation.getClass("fallbackFactory"));
+	}
+
+	/* for testing */ String getName(Map<String, Object> attributes) {
+		String name = (String) attributes.get("serviceId");
+		if (!StringUtils.hasText(name)) {
+			name = (String) attributes.get("name");
+		}
+		if (!StringUtils.hasText(name)) {
+			name = (String) attributes.get("value");
+		}
+		name = resolve(name);
+		return getName(name);
+	}
+
+	private String getContextId(Map<String, Object> attributes) {
+		String contextId = (String) attributes.get("contextId");
+		if (!StringUtils.hasText(contextId)) {
+			return getName(attributes);
+		}
+
+		contextId = resolve(contextId);
+		return getName(contextId);
+	}
+
+	private String resolve(String value) {
+		if (StringUtils.hasText(value)) {
+			return this.environment.resolvePlaceholders(value);
+		}
+		return value;
+	}
+
+	private String getUrl(Map<String, Object> attributes) {
+		String url = resolve((String) attributes.get("url"));
+		return getUrl(url);
+	}
+
+	private String getPath(Map<String, Object> attributes) {
+		String path = resolve((String) attributes.get("path"));
+		return getPath(path);
+	}
+
+	protected ClassPathScanningCandidateComponentProvider getScanner() {
+		return new ClassPathScanningCandidateComponentProvider(false, this.environment) {
+			@Override
+			protected boolean isCandidateComponent(
+					AnnotatedBeanDefinition beanDefinition) {
+				boolean isCandidate = false;
+				if (beanDefinition.getMetadata().isIndependent()) {
+					if (!beanDefinition.getMetadata().isAnnotation()) {
+						isCandidate = true;
+					}
+				}
+				return isCandidate;
+			}
+		};
+	}
+
+	/**
+	 * 获取 @EnableFeignClients中配置的 @FeignClient 接口扫描路径
+	 * @param importingClassMetadata
+	 * @return
+	 */
+	protected Set<String> getBasePackages(AnnotationMetadata importingClassMetadata) {
+		Map<String, Object> attributes = importingClassMetadata
+				.getAnnotationAttributes(EnableFeignClients.class.getCanonicalName());
+
+		Set<String> basePackages = new HashSet<>();
+		for (String pkg : (String[]) attributes.get("value")) {
+			if (StringUtils.hasText(pkg)) {
+				basePackages.add(pkg);
+			}
+		}
+		for (String pkg : (String[]) attributes.get("basePackages")) {
+			if (StringUtils.hasText(pkg)) {
+				basePackages.add(pkg);
+			}
+		}
+		for (Class<?> clazz : (Class[]) attributes.get("basePackageClasses")) {
+			basePackages.add(ClassUtils.getPackageName(clazz));
+		}
+
+		if (basePackages.isEmpty()) {
+			basePackages.add(
+					ClassUtils.getPackageName(importingClassMetadata.getClassName()));
+		}
+		return basePackages;
+	}
+
+	private String getQualifier(Map<String, Object> client) {
+		if (client == null) {
+			return null;
+		}
+		String qualifier = (String) client.get("qualifier");
+		if (StringUtils.hasText(qualifier)) {
+			return qualifier;
+		}
+		return null;
+	}
+
+	private String getClientName(Map<String, Object> client) {
+		if (client == null) {
+			return null;
+		}
+		String value = (String) client.get("contextId");
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("value");
+		}
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("name");
+		}
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("serviceId");
+		}
+		if (StringUtils.hasText(value)) {
+			return value;
+		}
+
+		throw new IllegalStateException("Either 'name' or 'value' must be provided in @"
+				+ FeignClient.class.getSimpleName());
+	}
+
+	private void registerClientConfiguration(BeanDefinitionRegistry registry, Object name,
+			Object configuration) {
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder
+				.genericBeanDefinition(FeignClientSpecification.class);
+		builder.addConstructorArgValue(name);
+		builder.addConstructorArgValue(configuration);
+		registry.registerBeanDefinition(
+				name + "." + FeignClientSpecification.class.getSimpleName(),
+				builder.getBeanDefinition());
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
+
+	/**
+	 * Helper class to create a {@link TypeFilter} that matches if all the delegates
+	 * match.
+	 *
+	 * @author Oliver Gierke
+	 */
+	private static class AllTypeFilter implements TypeFilter {
+
+		private final List<TypeFilter> delegates;
+
+		/**
+		 * Creates a new {@link AllTypeFilter} to match if all the given delegates match.
+		 * @param delegates must not be {@literal null}.
+		 */
+		AllTypeFilter(List<TypeFilter> delegates) {
+			Assert.notNull(delegates, "This argument is required, it must not be null");
+			this.delegates = delegates;
+		}
+
+		@Override
+		public boolean match(MetadataReader metadataReader,
+				MetadataReaderFactory metadataReaderFactory) throws IOException {
+
+			for (TypeFilter filter : this.delegates) {
+				if (!filter.match(metadataReader, metadataReaderFactory)) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+	}
+
+}
---
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/support/SpringEncoderTests.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.openfeign.support;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.List;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.openfeign.FeignContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+/**
+ * @author Spencer Gibb
+ * @author Olga Maciaszek-Sharma
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringEncoderTests.Application.class,
+		webEnvironment = WebEnvironment.RANDOM_PORT, value = {
+				"spring.application.name=springencodertest", "spring.jmx.enabled=false" })
+@DirtiesContext
+public class SpringEncoderTests {
+
+	@Autowired
+	private FeignContext context;
+
+	@Autowired
+	@Qualifier("myHttpMessageConverter")
+	private HttpMessageConverter<?> myConverter;
+
+	@Test
+	public void testCustomHttpMessageConverter() {
+		Encoder encoder = this.context.getInstance("foo", Encoder.class);
+		assertThat(encoder).isNotNull();
+		RequestTemplate request = new RequestTemplate();
+
+		encoder.encode("hi", MyType.class, request);
+
+		Collection<String> contentTypeHeader = request.headers().get("Content-Type");
+		assertThat(contentTypeHeader).as("missing content type header").isNotNull();
+		assertThat(contentTypeHeader.isEmpty()).as("missing content type header")
+				.isFalse();
+
+		String header = contentTypeHeader.iterator().next();
+		assertThat(header).as("content type header is wrong")
+				.isEqualTo("application/mytype");
+
+		assertThat(request.requestCharset()).as("request charset is null").isNotNull();
+		assertThat(request.requestCharset()).as("request charset is wrong")
+				.isEqualTo(Charset.forName("UTF-8"));
+	}
+
+	@Test
+	public void testBinaryData() {
+		Encoder encoder = this.context.getInstance("foo", Encoder.class);
+		assertThat(encoder).isNotNull();
+
+		RequestTemplate request = new RequestTemplate();
+
+		encoder.encode("hi".getBytes(), null, request);
+
+		assertThat(((List) request.headers().get(CONTENT_TYPE)).get(0))
+				.as("Request Content-Type is not octet-stream")
+				.isEqualTo(APPLICATION_OCTET_STREAM_VALUE);
+	}
+
+	@Test(expected = EncodeException.class)
+	public void testMultipartFile1() {
+		Encoder encoder = this.context.getInstance("foo", Encoder.class);
+		assertThat(encoder).isNotNull();
+		RequestTemplate request = new RequestTemplate();
+
+		MultipartFile multipartFile = new MockMultipartFile("test_multipart_file",
+				"hi".getBytes());
+		encoder.encode(multipartFile, MultipartFile.class, request);
+
+		assertThat(request.requestCharset()).as("request charset is not null").isNull();
+	}
+
+	// gh-105, gh-107
+	@Test
+	public void testMultipartFile2() {
+		Encoder encoder = this.context.getInstance("foo", Encoder.class);
+		assertThat(encoder).isNotNull();
+		RequestTemplate request = new RequestTemplate();
+		request.header(ACCEPT, MediaType.MULTIPART_FORM_DATA_VALUE);
+		request.header(CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE);
+
+		MultipartFile multipartFile = new MockMultipartFile("test_multipart_file",
+				"hi".getBytes());
+		encoder.encode(multipartFile, MultipartFile.class, request);
+
+		assertThat((String) ((List) request.headers().get(CONTENT_TYPE)).get(0))
+				.as("Request Content-Type is not multipart/form-data")
+				.contains("multipart/form-data; charset=UTF-8; boundary=");
+		assertThat(request.headers().get(CONTENT_TYPE).size())
+				.as("There is more than one Content-Type request header").isEqualTo(1);
+		assertThat(((List) request.headers().get(ACCEPT)).get(0))
+				.as("Request Accept header is not multipart/form-data")
+				.isEqualTo(MULTIPART_FORM_DATA_VALUE);
+		assertThat(((List) request.headers().get(CONTENT_LENGTH)).get(0))
+				.as("Request Content-Length is not equal to 186").isEqualTo("186");
+		assertThat(new String(request.requestBody().asBytes()))
+				.as("Body content cannot be decoded").contains("hi");
+	}
+
+	protected interface TestClient {
+
+	}
+
+	protected static class MyType {
+
+		private String value;
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableAutoConfiguration
+	@RestController
+	protected static class Application implements TestClient {
+
+		@Bean
+		HttpMessageConverter<?> myHttpMessageConverter() {
+			return new MyHttpMessageConverter();
+		}
+
+		private static class MyHttpMessageConverter
+				extends AbstractGenericHttpMessageConverter<Object> {
+
+			MyHttpMessageConverter() {
+				super(new MediaType("application", "mytype"));
+			}
+
+			@Override
+			protected boolean supports(Class<?> clazz) {
+				return false;
+			}
+
+			@Override
+			public boolean canRead(Class<?> clazz, MediaType mediaType) {
+				return true;
+			}
+
+			@Override
+			public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+				return clazz == String.class;
+			}
+
+			@Override
+			protected void writeInternal(Object o, Type type,
+					HttpOutputMessage outputMessage)
+					throws IOException, HttpMessageNotWritableException {
+
+			}
+
+			@Override
+			protected Object readInternal(Class<?> clazz, HttpInputMessage inputMessage)
+					throws IOException, HttpMessageNotReadableException {
+				return null;
+			}
+
+			@Override
+			public Object read(Type type, Class<?> contextClass,
+					HttpInputMessage inputMessage)
+					throws IOException, HttpMessageNotReadableException {
+				return null;
+			}
+
+		}
+
+	}
+
+	class MediaTypeMatcher implements ArgumentMatcher<MediaType> {
+
+		private MediaType mediaType;
+
+		MediaTypeMatcher(String type, String subtype) {
+			this.mediaType = new MediaType(type, subtype);
+		}
+
+		@Override
+		public boolean matches(MediaType argument) {
+			return this.mediaType.equals(argument);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuffer sb = new StringBuffer("MediaTypeMatcher{");
+			sb.append("mediaType=").append(this.mediaType);
+			sb.append('}');
+			return sb.toString();
+		}
+
+	}
+
+}
