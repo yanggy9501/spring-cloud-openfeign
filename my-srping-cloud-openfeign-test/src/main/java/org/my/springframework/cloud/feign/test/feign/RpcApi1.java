1b9eec0b8f79b1c24828de9d05796a63306a497a
yanggy <826449452@qq.com>
2025-11-14 20:19:42 +0800

    请求参数

--- my-srping-cloud-openfeign-test/src/main/java/org/my/springframework/cloud/feign/test/feign/RpcApi1.java
+++ my-srping-cloud-openfeign-test/src/main/java/org/my/springframework/cloud/feign/test/feign/RpcApi1.java
@@ -2,7 +2,10 @@ package org.my.springframework.cloud.feign.test.feign;
 
 import org.my.springframework.cloud.feign.test.vo.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Map;
 
 @FeignClient(name = "RpcApi1", url = "127.0.0.1:8080/hy")
 public interface RpcApi1 {
@@ -11,4 +14,12 @@ public interface RpcApi1 {
 
 	@GetMapping("/u")
 	User fu(User user);
+
+	@GetMapping("/u/{pathVar}")
+	User fu1(URI uri,
+			@RequestBody User user, // User user
+			@PathVariable("pathVar") String pathVar,
+			@RequestParam Map usr,
+			@RequestHeader Map header
+		);
 }
